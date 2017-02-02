b0612e152882edfa9d58409d1eb69fd21d3a69e2
Dan Murfitt <dan@murfitt.net>
2017-02-02 19:52:56 +0000

    Added test to check URL can be shortened.

--- tests/Integration/UrlTest.php
+++ tests/Integration/UrlTest.php
@@ -18,4 +18,21 @@ class UrlTest extends TestCase
         //Test an invalid url
         $this->assertFalse($url->isValid('http://bad_example.com'));
     }
+
+    /**
+     * @test
+     */
+    public function can_shorten_url() {
+        $originalUrl = 'http://example.com/section/subsection/excellent-article.php?autoplay=true#bestbit';
+
+        $url = new Url;
+        $shortenedUrl = $url->shorten($originalUrl);
+
+        //Check that it is not the same URL anymore
+        $this->assertNotEquals($originalUrl, $shortenedUrl);
+
+        //Check that the returned URL is short (defined by the domain + specified shortened path length)
+        $expectedShortenedUrlLength = strlen(APP_URL . '/') + SHORTENED_URL_PATH_LENGTH;
+        $this->assertEquals($expectedShortenedUrlLength, strlen($shortenedUrl));
+    }
 }
