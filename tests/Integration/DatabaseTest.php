5422409d47fa0db35f9bf1edcc975763f8a63102
Dan Murfitt <dan@murfitt.net>
2017-02-02 20:45:05 +0000

    Added test to make sure data can be properly inserted into the database

--- tests/Integration/DatabaseTest.php
+++ tests/Integration/DatabaseTest.php
@@ -6,11 +6,30 @@ use App\Library\Database;
 
 class DatabaseTest extends TestCase
 {
+    private $database;
+
+    protected function setUp() {
+        $this->database = new Database;
+    }
+
     /**
      * @test
      */
     public function can_run_a_query() {
-        $database = new Database;
-        $this->assertEquals(1, array_key_exists('version()', $database->query("select version();")->result()));
+        $this->assertEquals(1, array_key_exists('version()', $this->database->query("select version();")->result()));
+    }
+
+    /**
+     * @test
+     */
+    public function can_insert_into_database() {
+        $originalUrl = 'http://example.com';
+        $shortenedUrlPath = 'abcd123';
+
+        $this->database->query("INSERT INTO urls (url, path) VALUES (?, ?)", [$originalUrl, $shortenedUrlPath]);
+        $this->assertEquals(1, $this->database->affectedRows());
+
+        $this->database->query("SELECT * FROM urls WHERE url = '".$originalUrl."' AND path = '".$shortenedUrlPath."'");
+        $this->assertEquals(1, $this->database->affectedRows());
     }
 }
