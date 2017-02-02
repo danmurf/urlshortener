402befa6c0bf12afd5f66ffad12aa562de3d29bd
Dan Murfitt <dan@murfitt.net>
2017-02-02 22:36:36 +0000

    Added ability to submit URLs and have them shortened and displayed

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Url;
+
 Class HomeController
 {
     public function index() {
@@ -8,6 +10,17 @@ Class HomeController
     }
 
     public function store() {
-        echo 'storing';
+        $url = new Url;
+
+        if (!strlen($_POST['url']) > 0) {
+            $error = "The URL field is required";
+        }
+        elseif (!$url->isValid($url->addScheme($_POST['url']))) {
+            $error = "Please enter a valid URL";
+        }
+        else {
+            $shortenedUrl = $url->shorten($_POST['url']);
+        }
+        include '../resources/views/home.php';
     }
 }
--- resources/views/home.php
+++ resources/views/home.php
@@ -3,8 +3,27 @@
     <head>
         <meta charset="utf-8">
         <title><?=APP_NAME?></title>
+        <link rel="stylesheet" href="/styles.css">
     </head>
     <body>
         <h1><?=APP_NAME?></h1>
+
+        <?php if (!isset($shortenedUrl)) { ?>
+
+        <p>Enter a URL to shorten it:</p>
+        <form action="/" method="post">
+            <label for="url">URL</label>
+            <input id="url" type="text" name="url" value="">
+            <input type="submit" name="submit" value="Shorten">
+            <?php if (isset($error)) { ?>
+            <span id="error"><?=$error?><span>
+            <?php } ?>
+        </form>
+
+        <?php } else { ?>
+
+            <p>Your shortened URL is: <?=$shortenedUrl?></p>
+
+        <?php } ?>
     </body>
 </html>
