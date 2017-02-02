d1109625f372c205fb7d58848ffcbbad0a11dca2
Dan Murfitt <dan@murfitt.net>
2017-02-02 21:37:30 +0000

    Added support for testing database. Also, added tearDown for tests to truncate the database after completion

--- tests/SeleniumTestCase.php
+++ tests/SeleniumTestCase.php
@@ -2,7 +2,7 @@
 namespace App\Tests;
 
 require_once './vendor/autoload.php';
-require_once './.env.php';
+require_once './.env.testing.php';
 
 abstract class SeleniumTestCase extends \PHPUnit_Extensions_Selenium2TestCase
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,14 @@
 namespace App\Tests;
 
 require_once './vendor/autoload.php';
-require_once './.env.php';
+require_once './.env.testing.php';
+
+use App\Library\Database;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
+    protected function tearDown() {
+        $database = new Database;
+        $database->query("TRUNCATE TABLE urls;");
+    }
 }
