b7d876b8f7b9a8fec06a662225721c969bfccb79
Dan Murfitt <dan@murfitt.net>
2017-02-02 08:07:53 +0000

    Added Selenium

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,8 @@
 namespace App\Tests;
 
 require_once './vendor/autoload.php';
+require_once './.env.php';
 
-abstract class TestCase extends \PHPUnit_Framework_TestCase
+abstract class TestCase extends \PHPUnit_Extensions_Selenium2TestCase
 {
 }
