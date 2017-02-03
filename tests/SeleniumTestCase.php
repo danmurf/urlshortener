b877a645a68e0622e3b526e2acd9cb070fabe7c7
Dan Murfitt <dan@murfitt.net>
2017-02-03 19:42:13 +0000

    Added tearDown to Selenium test case

--- tests/SeleniumTestCase.php
+++ tests/SeleniumTestCase.php
@@ -4,6 +4,12 @@ namespace App\Tests;
 require_once './vendor/autoload.php';
 require_once './.env.testing.php';
 
+use App\Library\Database;
+
 abstract class SeleniumTestCase extends \PHPUnit_Extensions_Selenium2TestCase
 {
+    protected function tearDown() {
+        $database = new Database;
+        $database->query("TRUNCATE TABLE urls;");
+    }
 }
